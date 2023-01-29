afd3395298a23da8fe7a4ab52d404310c200f35c
Sanoj Fonseka <sanoj@boroughmarkets.com.au>
2023-01-29 16:28:39 +0530

    new11

--- tests/test_tasks_processes.py
+++ tests/test_tasks_processes.py
@@ -19,7 +19,7 @@ from src._run_scripts_ import *
 warnings.filterwarnings('ignore')
 
 @pytest.fixture(scope="session")
-def create_spark_session():
+def create_spark_session(request):
 
     """
     Create spark session
@@ -98,7 +98,7 @@ def df_equality(df1, df2):
     return True
 
 logger = custom_logger(app_name = 'HelloFresh | Data Engineer | Unit Tests | Sanoj Fonseka | ')
-spark = create_spark_session()
+spark = pytest.mark.usefixtures("create_spark_session")
 
 def create_test_input_data_for_unit_testing(spark):
 
